6d5af42897f9f919fbc08d4ac0823dd93e2bcd5c
harrison <harrisonpayesa@gmail.com>
2025-08-05 02:10:47 -0500

    Primera versión terminada del proyecto

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import requests
 from datetime import datetime
 from app_config import *
 
-WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
-
+# Funcion para colocar el clima de manera mas descriptiva
 def clima(clima_id):
     if clima_id >= 200 and clima_id <= 232:
         return "Tormenta 🌩️"
@@ -21,14 +20,18 @@ def clima(clima_id):
         emojis = ["","🌤️","⛅","🌥️", "☁️"]
         return f"Nuboso {emojis[clima_id - 800]}"
 
+
+WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+
 weather_params = {
     "lon" : LONG_POSITION,
     "lat" : LAT_POSTITION,
     "units" : "metric",
-    "cnt" : 8,
+    "cnt" : 8, # aproximado de datos del clima por dia
     "appid" : API_KEY
 }
 
+# Haciendo el llamado a la API del clima
 response_weather = requests.get(WEATHER_API_ENDPOINT, params=weather_params)
 response_weather.raise_for_status()
 data = response_weather.json()
@@ -42,11 +45,9 @@ hoy = datetime.now().strftime('%Y-%m-%d')
 pronosticos_hoy = [pronostico for pronostico in lista_pronosticos if pronostico['dt_txt'].startswith(hoy)]
 pronosticos_hoy.append(lista_pronosticos[len(pronosticos_hoy)])
 
+# Creacion del mensaje a enviar
 mensaje = "Pronóstico del clima:\n"
-
-# mensaje += f"Pais: {pais}\nCiudad: {city_name}\nFecha: {hoy}\n"
 mensaje += f"{hoy} {city_name} {pais}\n"
-# print(f"Pais: {pais}\nCiudad: {city_name}\nFecha: {hoy}")
 
 for pronostico in pronosticos_hoy:
     tiempo = pronostico['dt_txt'].split(" ")
@@ -56,21 +57,19 @@ for pronostico in pronosticos_hoy:
     clima_id = int(pronostico["weather"][0]["id"])
     clima_ahora = clima(clima_id)
     mensaje += f"{hora_12} {temperatura}° {clima_ahora}\n"
-    # print(f"{hora_12} - {temperatura}° - {clima_ahora}")
 
-
-# print(pronosticos_hoy)
 
 TELEGRAM_API_ENDPOINT = f'https://api.telegram.org/bot{TELEGRAM_BOT_KEY}/sendMessage'
 telegram_params = {
-    'chat_id': TELEGRAM_USER,
+    'chat_id': TELEGRAM_USER, # empezar la conversación con el bot preciviamente en telegram
     'text': mensaje
 }
 
+# Haciendo el llamado a la API de telegram
 response_telegram = requests.post(TELEGRAM_API_ENDPOINT, data=telegram_params)
 
+# Verificacion de la llegada del mensaje
 if response_telegram.status_code == 200:
     print("Mensaje enviado correctamente")
 else:
     print("Error al enviar el mensaje", response_telegram.text)
-
